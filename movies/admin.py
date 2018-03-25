71853fffb2c978245e6b23f3cdfecd5d6402ebf0
bart3k1 <bart3k1@gmail.com>
2018-03-25 14:34:21 +0200

    0.0.0.2

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from movies.models import Person, Movie, Role
+
+
+class RoleInline(admin.TabularInline):
+    model = Movie.actors.through
+
+# class RoleInline(admin.StackedInline):
+#     model = Movie.actors.through
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("name", "surname")
+    inlines = [
+        RoleInline,
+    ]
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "director", "year")
+    inlines = [
+        RoleInline,
+    ]
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name + " - " + self.surname
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=64, verbose_name="Tytuł")
+    director = models.ForeignKey(Person, on_delete=models.CASCADE)
+    actors = models.ManyToManyField(Person, related_name="persons", through="Role")
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class Role(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    role_name = models.CharField(max_length=128)
